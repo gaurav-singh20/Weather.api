c819479151be9f0dd816da64fd4c349e2be5c5ee
Amogh Krishna <amoghkrishna55@gmail.com>
2024-04-24 00:03:07 +0530

    Refactor weather API integration in index.js

--- index.js
+++ index.js
@@ -7,47 +7,47 @@ const pb = new PocketBase("https://linkify.pockethost.io/");
 const app = express();
 const PORT = 8080;
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
+const getWeather = async (city) => {
+  let response = await axios.get(
+    `http://api.weatherapi.com/v1/current.json?key=6c5c3579842f4dab8af195057241704&q=${city}&aqi=no`
+  );
+  if (response.data && response.data.location) {
+    let data = response.data;
+    const newData = {
+      name: data.location.name,
+      temp: data.current.temp_c,
+      cloud: data.current.cloud,
+      humidity: data.current.humidity,
+      wind: data.current.wind_kph,
+      condition: data.current.condition.text,
+      code: data.current.condition.code,
+      date: data.location.localtime,
+      is_day: data.current.is_day,
+    };
+    try {
+      const record = await pb
+        .collection("weather")
+        .getFirstListItem(`name = "${newData.name}"`);
+      await pb.collection("weather").update(record.id, newData);
+      console.log("UPDATED");
+    } catch {
+      await pb.collection("weather").create(newData);
+      console.log("CREATED");
+    }
+    return newData;
+  } else {
+    console.log("Retrying");
+    return getWeather(city);
+  }
+};
 
 app.get("/:city", async (req, res) => {
   const { city } = req.params;
   try {
-    let response = await axios.get(
-      `http://api.weatherapi.com/v1/current.json?key=6c5c3579842f4dab8af195057241704&q=${city}&aqi=no`
-    );
-    if (response.data && response.data.location) {
-      let data = response.data;
-      console.log(data);
-      const newData = {
-        name: data.location.name,
-        temp: data.current.temp_c,
-        cloud: data.current.cloud,
-        humidity: data.current.humidity,
-        wind: data.current.wind_kph,
-        condition: data.current.condition.text,
-        code: data.current.condition.code,
-        date: data.location.localtime,
-        is_day: data.current.is_day,
-      };
-      try {
-        const record = await pb
-          .collection("weather")
-          .getFirstListItem(`name = "${newData.name}"`);
-        await pb.collection("weather").update(record.id, newData);
-        console.log("UPDATED");
-      } catch {
-        await pb.collection("weather").create(newData);
-        console.log("CREATED");
-      }
-      res.send(newData);
-    } else {
-      res.send({ error: "Invalid response from weather API" });
-    }
+    const data = await getWeather(city);
+    res.send(data);
   } catch (error) {
-    console.error(error);
+    console.log("error");
     res.send({ error: "Error fetching weather data" });
   }
 });
